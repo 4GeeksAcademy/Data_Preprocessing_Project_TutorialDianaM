50c24a9c8923538fee27485753250c767edddb17
dianamonroe <dianamonroe79@gmail.com>
2024-11-17 17:50:06 +0000

    Finsihed

--- src/app.py
+++ src/app.py
@@ -1,5 +1,13 @@
 from utils import db_connect
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
 
 engine = db_connect()
-
-# your code here
+PATH = os.getcwd()
+data_path = os.path.join(PATH, '/workspaces/Data_Preprocessing_Project_TutorialDianaM/data/raw/AB_NYC_2019.csv')
+df = pd.read_csv(data_path)
